dad8f24aaf49e6779eb7ca9a0834cc5636568ae8
rach-18 <rachitapradhan448@gmail.com>
2024-08-06 18:19:26 +0530

    Made minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,24 +11,28 @@ function Header() {
   };
 
   return (
-    <header className='flex justify-between px-10 py-5 bg-[#F8F9FA] items-center'>
-      <Link to='/'><HomeIcon sx={{fontSize: 40, color: '#0D6EFD'}} /></Link>
-      <p className='text-4xl text-[#6c757d] font-bold'>Search properties to rent</p>
-      <div className='flex items-center gap-5'>
+    <header className='flex flex-col md:flex-row justify-between px-4 md:px-10 py-5 bg-[#F8F9FA] items-center'>
+      <div className='flex flex-col md:flex-row justify-between w-full md:w-auto items-center mb-4 md:mb-0'>
+        <Link to='/'><HomeIcon sx={{ fontSize: 40, color: '#0D6EFD' }} /></Link>
+        <p className='text-xl lg:text-4xl text-[#6c757d] font-bold ml-2 md:mx-2'>Search properties to rent</p>
+      </div>
+      <div className='flex flex-col md:flex-row items-center gap-2 md:gap-5 w-full md:w-auto'>
         <input 
           type="text" 
-          className='border-[0.1rem] border-[#DEE2E6] p-2 rounded-lg' 
+          className='border-[0.1rem] border-[#DEE2E6] p-2 rounded-lg w-full md:w-auto' 
           placeholder='Search...'
           value={searchQuery}
           onChange={handleSearchChange}
         />
-        <Link to='/' className='tran border-[0.1rem] border-[#212529] py-2 px-4 rounded-lg hover:bg-[#212529] hover:text-white'>Search</Link>
-        <Link
-          to='/liked' 
-          className='tran border-[0.1rem] border-[#212529] py-2 px-4 rounded-lg hover:bg-[#212529] hover:text-white'
-        >
-          Liked
-        </Link>
+        <div className='flex gap-2 md:gap-5 w-full md:w-auto'>
+          <Link to='/' className='tran border-[0.1rem] border-[#212529] py-2 px-4 rounded-lg hover:bg-[#212529] hover:text-white w-full md:w-auto text-center'>Search</Link>
+          <Link
+            to='/liked' 
+            className='tran border-[0.1rem] border-[#212529] py-2 px-4 rounded-lg hover:bg-[#212529] hover:text-white w-full md:w-auto text-center'
+          >
+            Liked
+          </Link>
+        </div>
       </div>
     </header>
   )
